6822b48f6c9e6c4c5b17a527497b34e2826869d3
cekonsb <806138244@qq.com>
2020-03-04 14:40:09 +0800

    fix retestnet genesis hash

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -414,10 +414,10 @@ public:
         nDefaultPort = 19994;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1417713337, 1096447, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1583294400, 6541416, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
+        assert(consensus.hashGenesisBlock == uint256S("0x0000079b61cc19740ef65f3c6f28ba16dee8231cfc5e30034bf5b0b6ddcf2265"));
+        assert(genesis.hashMerkleRoot == uint256S("0x7d022b845e153f53724a724d09676d12c440bfde9a1f0e754fe0a6881d4ff498"));
 
         vFixedSeeds.clear(); //! Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();  //! Regtest mode doesn't have any DNS seeds.
@@ -432,7 +432,7 @@ public:
 
         checkpointData = (CCheckpointData){
             boost::assign::map_list_of
-            ( 0, uint256S("0x000008ca1832a4baf228eb1553c03d3a2c8e02399550dd6ea8d65cec3ef23d2e")),
+            ( 0, uint256S("0x0000079b61cc19740ef65f3c6f28ba16dee8231cfc5e30034bf5b0b6ddcf2265")),
             0,
             0,
             0
